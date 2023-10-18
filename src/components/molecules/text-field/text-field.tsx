86f814b0bfdd14af64c0b68ccf2b6aaa45d937bf
yagofrancia <yagofrancia@hotmail.com>
2023-10-18 20:06:04 -0300

    adding inputs

--- src/components/molecules/text-field/text-field.tsx
+++ src/components/molecules/text-field/text-field.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
-import {TextInput} from 'react-native';
+import {
+  NativeSyntheticEvent,
+  TextInput,
+  TextInputChangeEventData,
+} from 'react-native';
 import styles from './styles';
 
 type TextFieldProps = {
   editable?: boolean;
+  value: string;
+  onChange: (e: string | React.ChangeEvent<any>) => void;
 };
 
-export default function TextField({editable}: TextFieldProps) {
-  return <TextInput editable={editable} style={styles.root} />;
+export default function TextField({editable, value, onChange}: TextFieldProps) {
+  function handleChange(e: NativeSyntheticEvent<TextInputChangeEventData>) {
+    onChange(e as unknown as React.ChangeEvent<any>);
+  }
+  return (
+    <TextInput
+      editable={editable}
+      style={styles.root}
+      value={value}
+      onChange={handleChange}
+    />
+  );
 }
--- src/components/organisms/create-account-form/create-account-form.tsx
+++ src/components/organisms/create-account-form/create-account-form.tsx
@@ -4,27 +4,71 @@ import styles from './styles';
 import LabeledInput from '../../molecules/labeled-input/labeled-input';
 import Select from '../../molecules/select';
 import TextField from '../../molecules/text-field/text-field';
+import {useFormik} from 'formik';
+
+type FormShape = {
+  parentAccount: string;
+  code: string;
+  name: string;
+  isRevenue: boolean;
+  launch: boolean;
+};
 
 export default function CreateAccountForm() {
-  const [data, setData] = React.useState('');
+  const formik = useFormik<FormShape>({
+    initialValues: {
+      parentAccount: '',
+      code: '',
+      name: '',
+      isRevenue: false,
+      launch: false,
+    },
+    onSubmit: handleSubmit,
+  });
+
+  function handleSubmit() {}
 
   return (
     <View style={styles.root}>
       <LabeledInput label="Conta pai">
-        <Select onChange={value => setData(value)} value={data}>
+        <Select
+          onChange={formik.handleChange('parentAccount')}
+          value={formik.values.parentAccount}>
+          <Select.Item value="" label="Selecione uma conta pai" />
           <Select.Item value="option 1" label="Option 1" />
           <Select.Item value="option 2" label="Option 2" />
           <Select.Item value="option 3" label="Option 3" />
+          {/* Dynamically filled */}
         </Select>
       </LabeledInput>
       <LabeledInput label="Código">
-        <TextField />
+        <TextField
+          value={formik.values.code}
+          onChange={formik.handleChange('code')}
+        />
       </LabeledInput>
       <LabeledInput label="Nome">
-        <TextField />
+        <TextField
+          value={formik.values.name}
+          onChange={formik.handleChange('name')}
+        />
       </LabeledInput>
-      <LabeledInput label="Tipo">{/* <Select /> */}</LabeledInput>
-      <LabeledInput label="Aceita lançamentos">{/* <Select /> */}</LabeledInput>
+      <LabeledInput label="Tipo">
+        <Select
+          onChange={formik.handleChange('isRevenue')}
+          value={formik.values.isRevenue}>
+          <Select.Item value={true} label="Receita" />
+          <Select.Item value={false} label="Custo" />
+        </Select>
+      </LabeledInput>
+      <LabeledInput label="Aceita lançamentos">
+        <Select
+          onChange={formik.handleChange('launch')}
+          value={formik.values.launch}>
+          <Select.Item value={false} label="Não" />
+          <Select.Item value={true} label="Sim" />
+        </Select>
+      </LabeledInput>
     </View>
   );
 }
